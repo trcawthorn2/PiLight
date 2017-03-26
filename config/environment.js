9009c7be3dbd99f5402a3e2f7a2490cb39442d42
Travis Cawthorn <admin@Admins-MacBook-Pro.local>
2017-03-26 11:51:25 -0400

    Changed name to match git hub repo

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'pie-light-controller',
+    modulePrefix: 'PiLight',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
